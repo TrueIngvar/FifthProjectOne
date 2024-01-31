d6688b11f6f97efb9d2f04b7a9af490e941b8c35
TrueIngvar <igor171298@mail.ru>
2024-01-31 18:38:42 +0400

    A successful code has been written

--- src/test/java/ru/netology/services/RestCounterServiceTest.java
+++ src/test/java/ru/netology/services/RestCounterServiceTest.java
@@ -1,22 +1,23 @@
 package ru.netology.services;
-/*import ru.netology.services.RestCounterService;*/
-/*import org.junit.jupiter.api.Test;*/
-/*import org.junit.jupiter.api.Assertions;*/
+//import ru.netology.services.RestCounterService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class RestCounterServiceTest {
-    /*@Test*/
+    @Test
     void countMonth() {
 
         RestCounterService service = new RestCounterService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int money = 0; // количество денег на счету
-        int takeRest =  money - expenses - ((money - expenses) / 3);
+        long money = 0; // количество денег на счету
+        long income = 10_000;
+        long expenses = 3_000;
+        long threshold = 20_000;
+        //long takeRest = (money - expenses) - ((money - expenses) / 3);
         long expected = 3;
 
-        //long rest = service.calculate(money, income, expenses, takeRest, threshold);
 
-        /*Assertions.assertEquals(expected, rest);*/
+        long rest = service.calculate(money, income, expenses, threshold/*, takeRest*/);
+        Assertions.assertEquals(expected, rest);
     }
 }
