c0c1899c5a938abed933b7e6596b44f3d6d13ca5
TrueIngvar <igor171298@mail.ru>
2024-01-31 18:43:10 +0400

    A code's been checked with another one conditions

--- src/test/java/ru/netology/services/RestCounterServiceTest.java
+++ src/test/java/ru/netology/services/RestCounterServiceTest.java
@@ -10,11 +10,11 @@ public class RestCounterServiceTest {
 
         RestCounterService service = new RestCounterService();
         long money = 0; // количество денег на счету
-        long income = 10_000;
-        long expenses = 3_000;
-        long threshold = 20_000;
+        long income = 100_000;
+        long expenses = 60_000;
+        long threshold = 150_000;
         //long takeRest = (money - expenses) - ((money - expenses) / 3);
-        long expected = 3;
+        long expected = 2;
 
 
         long rest = service.calculate(money, income, expenses, threshold/*, takeRest*/);
