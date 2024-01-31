e207cf050faf725eefa44c9ab2154b5b4be25546
TrueIngvar <igor171298@mail.ru>
2024-01-31 18:58:12 +0400

    Completed 2nd task with CSV file

--- src/test/java/ru/netology/services/RestCounterServiceTest.java
+++ src/test/java/ru/netology/services/RestCounterServiceTest.java
@@ -3,18 +3,21 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestCounterServiceTest {
-    @Test
-    void countMonth() {
+    @ParameterizedTest
+    @CsvFileSource (files="src/test/resources/inputData.csv")
+    void countMonth(long money, long income, long expenses, long threshold, long expected) {
 
         RestCounterService service = new RestCounterService();
-        long money = 0; // количество денег на счету
-        long income = 100_000;
-        long expenses = 60_000;
-        long threshold = 150_000;
+        //long money = 0; // количество денег на счету
+        //long income = 100_000;
+        //long expenses = 60_000;
+        //long threshold = 150_000;
         //long takeRest = (money - expenses) - ((money - expenses) / 3);
-        long expected = 2;
+        //long expected = 2;
 
 
         long rest = service.calculate(money, income, expenses, threshold/*, takeRest*/);
